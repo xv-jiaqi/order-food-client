b4fd613489fee3431a39f33431da2114243a1a0d
linzhongming <lin.zh.ming.27@gmail.com>
2017-05-21 01:52:57 +0800

    升级angular-cli版本，解决build后挂掉的问题

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, NgModule} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  user = {
+    name: '王大锤'
+  };
+
+  userList = [{
+    username: '王大锤'
+  }, {
+    username: '王二锤'
+  }];
 }
