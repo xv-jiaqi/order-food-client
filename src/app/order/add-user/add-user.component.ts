64724fbf1b24d3c94a8c88faabb1ef051a13985d
kkxujq <kkxujq@163.com>
2017-09-18 18:29:19 +0800

    :sparkles: 校验

--- src/app/order/add-user/add-user.component.ts
+++ src/app/order/add-user/add-user.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+
+const USERNAME_MAX_LENGTH = 10;
+const USERNAME_MIN_LENGTH = 4;
+const USERNAME_REGEX = /^[a-zA-Z]{1}([a-zA-Z0-9]|[._]){3,9}$/;
+
+const PASSWORD_MAX_LENGTH = 20;
+const PASSWORD_MIN_LENGTH = 6;
+const PASSWORD_REGEX = /^(\w){6,20}$/;
 
 @Component({
   selector: 'app-add-user',
@@ -11,9 +21,42 @@ export class AddUserComponent implements OnInit {
   password: string;
   againPassword: string;
 
+  lengthLimit = null;
+
   constructor() { }
 
   ngOnInit() {
+    this.username = '';
+    this.password = '';
+    this.lengthLimit = {
+      USERNAME_MAX_LENGTH,
+      USERNAME_MIN_LENGTH,
+      PASSWORD_MAX_LENGTH,
+      PASSWORD_MIN_LENGTH
+    };
   }
 
+  usernameFormControl = new FormControl('', [
+    Validators.required,
+    Validators.pattern(USERNAME_REGEX),
+    Validators.maxLength(USERNAME_MAX_LENGTH),
+    Validators.minLength(USERNAME_MIN_LENGTH)
+  ]);
+  passwordFormControl = new FormControl('', [
+    Validators.required,
+    Validators.pattern(PASSWORD_REGEX),
+    Validators.maxLength(PASSWORD_MAX_LENGTH),
+    Validators.minLength(PASSWORD_MIN_LENGTH)
+  ]);
+  againPasswordFormControl = new FormControl('', [
+    Validators.required,
+    Validators.pattern(new RegExp(`^${this.password}$`))
+  ]);
+
+  submit(): void {
+    console.log(this.username);
+  }
+
+  cancel(): void {
+  }
 }
